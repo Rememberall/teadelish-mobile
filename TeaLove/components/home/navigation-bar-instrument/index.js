d7f4303fba9895146ea4f02f490636a796d188d4
beeccy <beeccy93@gmail.com>
2017-02-03 19:55:09 +0100

    Added stuff

---
+++ TeaLove/components/home/navigation-bar-instrument/index.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+import NavigationBar from '../../navigation-bar';
+
+import * as style from './style-instrument';
+
+const NavigationBarInstrument = () => (
+  <View>
+    <NavigationBar />
+  </View>
+)
+
+export default NavigationBar;
